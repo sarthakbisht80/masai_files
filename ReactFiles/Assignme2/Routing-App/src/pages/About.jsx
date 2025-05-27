0a267c98e38f5600fb012f380717939de0bda14e
Sarthak Bisht <bishtsarthak80@gmail.com>
2025-05-27 22:20:58 +0530

    Blog app added

---
+++ ReactFiles/Assignme2/Routing-App/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About This Project</h2>
+      <p>This is a simple React blog app made by Ricky.</p>
+    </div>
+  );
+}
+
+export default About;
