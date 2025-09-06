3b14d2b24e64e6592d8bde23f4e5af5c8453a414
Sarthak Bisht <bishtsarthak80@gmail.com>
2025-09-06 18:04:18 +0530

    ques 4 added

---
+++ oops/ts/DesignPattern/Ques4.js
@@ -0,0 +1,41 @@
+var Bike = /** @class */ (function () {
+    function Bike() {
+    }
+    Bike.prototype.start = function () {
+        console.log("Bike is starting");
+    };
+    return Bike;
+}());
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.start = function () {
+        console.log("Car is starting");
+    };
+    return Car;
+}());
+var Driver = /** @class */ (function () {
+    function Driver(vehicle) {
+        this.vehicle = vehicle;
+    }
+    Driver.prototype.drive = function () {
+        this.vehicle.start();
+        console.log("Driving...");
+    };
+    Driver.prototype.setVehicle = function (vehicle) {
+        this.vehicle = vehicle;
+    };
+    return Driver;
+}());
+var bike = new Bike();
+var car = new Car();
+var driver = new Driver(bike);
+driver.drive();
+// Output:
+// Bike is starting
+// Driving...
+driver.setVehicle(car);
+driver.drive();
+// Output:
+// Car is starting
+// Driving...
